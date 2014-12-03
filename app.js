8f47423d2684e4458f0b86ce810bf07ced0b2486
Delmer Reed <delmer814@yahoo.com>
2014-12-03 14:22:17 -0800

    cool stuf

--- app.js
+++ app.js
@@ -13,9 +13,27 @@ app.get("/search", function (req, res) {
 	var title = req.query.movieTitle;
 	request('http://www.omdbapi.com/?s=' + title, function (err, response, body) {
 		console.log(body);
-		res.send(body);
+    var results = JSON.parse(body);
+    var searchResults = results.Search;
+		res.render("search", {movieList: searchResults});
 	});
 });
+
+
+app.get("/movie/:id", function (req, res) {
+  var id = req.params.id;
+  request('http://www.omdbapi.com/?i=' + id, function (err, response, body) {
+    console.log(body);
+    var movie = JSON.parse(body);
+    res.render("show.ejs", {movieInfo: movie});
+  });
+});
+
+
+
+
+
+
 
 app.listen(3000, function () {
 	console.log("Visit localhost:3000");
